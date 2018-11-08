b1fe3e2600837894f65c0d5ba1ce0b70966f4654
Julia Kornich <juliako@microsoft.com>
2018-11-07 21:06:00 -0800

    Update Program.cs

--- AMSV3Tutorials/EncryptWithAES/Program.cs
+++ AMSV3Tutorials/EncryptWithAES/Program.cs
@@ -291,6 +291,9 @@ namespace EncryptWithAES
                 // You may want to update this part to throw an Exception instead, and handle name collisions differently.
                 string uniqueness = $"-{Guid.NewGuid().ToString("N")}";
                 outputAssetName += uniqueness;
+                
+                Console.WriteLine("Warning – found an existing Asset with name = " + assetName);
+                Console.WriteLine("Creating an Asset with this name instead: " + outputAssetName);                
             }
 
             return await client.Assets.CreateOrUpdateAsync(resourceGroupName, accountName, outputAssetName, asset);
