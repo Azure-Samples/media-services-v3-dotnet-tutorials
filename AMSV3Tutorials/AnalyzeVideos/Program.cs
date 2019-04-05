d9a3861a2eeced458c4fee2cf42ed1856f2e4656
Julia Kornich <juliako@microsoft.com>
2019-04-04 21:03:48 -0700

    Update Program.cs

--- AMSV3Tutorials/AnalyzeVideos/Program.cs
+++ AMSV3Tutorials/AnalyzeVideos/Program.cs
@@ -119,10 +119,6 @@ namespace AnalyzeVideos
         // <GetCredentialsAsync>
         private static async Task<ServiceClientCredentials> GetCredentialsAsync(ConfigWrapper config)
         {
-            // Use UserTokenProvider.LoginWithPromptAsync or UserTokenProvider.LoginSilentAsync to get a token using user authentication
-            //// ActiveDirectoryClientSettings.UsePromptOnly
-            //// UserTokenProvider.LoginWithPromptAsync
-
             // Use ApplicationTokenProvider.LoginSilentWithCertificateAsync or UserTokenProvider.LoginSilentAsync to get a token using service principal with certificate
             //// ClientAssertionCertificate
             //// ApplicationTokenProvider.LoginSilentWithCertificateAsync
