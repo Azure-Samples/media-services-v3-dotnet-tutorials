71b80865068768184e2bee8fea2ebef593f21f60
Julia Kornich <juliako@microsoft.com>
2019-04-04 21:05:58 -0700

    Update Program.cs

--- AMSV3Tutorials/UploadEncodeAndStreamFiles/Program.cs
+++ AMSV3Tutorials/UploadEncodeAndStreamFiles/Program.cs
@@ -123,10 +123,6 @@ namespace UploadEncodeAndStreamFiles
         // <GetCredentialsAsync>
         private static async Task<ServiceClientCredentials> GetCredentialsAsync(ConfigWrapper config)
         {
-            // Use UserTokenProvider.LoginWithPromptAsync or UserTokenProvider.LoginSilentAsync to get a token using user authentication
-            //// ActiveDirectoryClientSettings.UsePromptOnly
-            //// UserTokenProvider.LoginWithPromptAsync
-
             // Use ApplicationTokenProvider.LoginSilentWithCertificateAsync or UserTokenProvider.LoginSilentAsync to get a token using service principal with certificate
             //// ClientAssertionCertificate
             //// ApplicationTokenProvider.LoginSilentWithCertificateAsync
