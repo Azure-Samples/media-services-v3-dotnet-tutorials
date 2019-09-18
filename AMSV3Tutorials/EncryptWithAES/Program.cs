2cf96daa8926c5a3223e8980e404d88875048ff1
Julia Kornich <juliako@microsoft.com>
2019-09-18 16:46:05 -0700

    Update Program.cs

--- AMSV3Tutorials/EncryptWithAES/Program.cs
+++ AMSV3Tutorials/EncryptWithAES/Program.cs
@@ -537,8 +537,6 @@ namespace EncryptWithAES
             string assetName,
             string outputFolderName)
         {
-            const int ListBlobsSegmentMaxResult = 5;
-
             if (!Directory.Exists(outputFolderName))
             {
                 Directory.CreateDirectory(outputFolderName);
@@ -564,6 +562,11 @@ namespace EncryptWithAES
 
             do
             {
+                // A non-negative integer value that indicates the maximum number of results to be returned at a time,
+                // up to the per-operation limit of 5000. If this value is null, the maximum possible number of results
+                // will be returned, up to 5000.
+                int? ListBlobsSegmentMaxResult = null;    
+                
                 BlobResultSegment segment = await container.ListBlobsSegmentedAsync(null, true, BlobListingDetails.None, ListBlobsSegmentMaxResult, continuationToken, null, null);
 
                 foreach (IListBlobItem blobItem in segment.Results)
